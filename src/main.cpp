a2b83e6ec35a7ad4b8778c849d566371525e5643
Jihoo Kim <jihookim216@gmail.com>
2024-12-02 16:01:58 +0900

    Create Pokemon class for managing Pokemons

--- src/main.cpp
+++ src/main.cpp
@@ -13,3 +13,20 @@ class Skill{
     int skilldmg;
     int maxtry;
 };
+class Pokemon{
+    public:
+    Pokemon(const string& name, const string& type, const int& hp);
+    Pokemon();
+
+    void setskill(int skillnum, string name, string type, int dmg, int max){
+        skills[skillnum].skilledit(name, type, dmg, max);
+    }
+
+    void showstats();
+
+    private:
+    string name;
+    string type;
+    int maxhp;
+    Skill skills[4];
+};
